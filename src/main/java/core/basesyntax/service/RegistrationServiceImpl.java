658c245e24129b6698a79705416d2555e7f7ebbf
Bohdan Vorobiei <vbs@ukr.net>
2026-02-04 02:44:18 +0200

    task is being performed so far

--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -9,6 +9,27 @@ public class RegistrationServiceImpl implements RegistrationService {
 
     @Override
     public User register(User user) {
-        return null;
+        if (user == null) {
+            throw new RuntimeException("User is null");
+        }
+
+        if (user.getLogin() == null || user.getLogin().length() < 6) {
+            throw new RuntimeException("Login is invalid");
+        }
+
+        if (user.getPassword() == null || user.getPassword().length() < 6) {
+            throw new RuntimeException("Password is invalid");
+        }
+
+        if (user.getAge() == null || user.getAge() < 18) {
+            throw new RuntimeException("Age is invalid");
+        }
+
+        if (storageDao.get(user.getLogin()) != null) {
+            throw new RuntimeException("User already exists");
+        }
+
+        return storageDao.add(user);
     }
 }
+
